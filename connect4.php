8ceeb86fc5397a700ccb465a044c5d576b8cd5ad
vaggos542 <37916133+vaggos542@users.noreply.github.com>
2021-01-03 00:01:46 +0200

    Update connect4.php

--- connect4.php
+++ connect4.php
@@ -17,23 +17,32 @@ $input = json_decode(file_get_contents('php://input'), true);
 
 switch ($r = array_shift($request)) {
 	
-	case 'board':
-		switch ($b=array_shift($request)) {
-			case '':
-			case null: handle_board($method);
-			break;
+//check if board exists
+    case 'board' :
+        switch ($b=array_shift($request)) {
+            case '':
+            case null: handle_board($method);
+                break;
+            case 'piece': handle_piece($method, $input);
+                break;
+            case 'player': handle_player($method, $input);
+                break;
+            default: header("HTTP/1.1 404 Not Found");
+                break;
+        }
+        break;
 
-			default: header("HTTP/1.1 404 Not Found");
-            break;
-		}
-   	break;
-    
+//check if status exists
     case 'status':
 		if(sizeof($request)==0) {show_status();}
 		else {header("HTTP/1.1 404 Not Found");}
 		break;
     
-
+//check if players exists
+	case 'players':		
+		handle_player($method, $request,$input);
+        break;
+    
     default:
         header("HTTP/1.1 404 Not Found");
         exit;
@@ -45,7 +54,35 @@ function handle_board($method) {
                 show_board();
         } else if ($method=='POST') {
                 reset_board();
-        }
+       }
 		
 }
 
+
+//player validation
+function handle_player($method, $request, $input) {
+	switch ($r=array_shift($request)) {
+		case '':
+		case null: if($method=='GET') {show_players($method);}
+				   else {header("HTTP/1.1 400 Bad Request"); 
+						 print json_encode(['errormesg'=>"Method $method not allowed here."]);}
+                    break;
+        case 'R': 
+		case 'Y': handle_user($method, $r, $input);
+				  break;		
+		default: header("HTTP/1.1 404 Not Found");
+				 print json_encode(['errormesg'=>"Player $r not found."]);
+                 break;
+	}
+}
+
+
+function handle_piece($method, $input) {
+    if($method=='GET') {
+        show_board_piece();
+    }else if($method=='POST') {
+        move($input);
+    }
+}
+
+?>
