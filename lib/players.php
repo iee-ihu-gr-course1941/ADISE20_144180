0963cda67a78cb9d59d53e0ade395ac5d0e4dc0e
vaggos542 <37916133+vaggos542@users.noreply.github.com>
2021-01-04 14:24:52 +0200

    Update players.php

--- lib/players.php
+++ lib/players.php
@@ -78,20 +78,7 @@ function handle_user($method, $b,$input) {
     }
 }
 
-function current_color($token) {
 
-	global $mysqli;
-	if($token==null) {return(null);}
-	$sql = 'select * from con4.players where token=?';
-	$st = $mysqli->prepare($sql);
-	$st->bind_param('s',$token);
-	$st->execute();
-	$res = $st->get_result();
-	if($row=$res->fetch_assoc()) {
-		return($row['piece_color']);
-	}
-	return(null);
-}
 
 
 ?>
